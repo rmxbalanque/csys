07f38404fd7e098655f2eef36f665edd02433d02
Tristan <tristan.bouchard@digipen.edu>
2020-06-10 20:50:46 -0700

    fix:fixed boolean argument not being parsed and fixed more things with vector parsing
    __DESCRIPTION:__
    Changed a || into an &&, rewrote vector argument parsing. issue will come up where string ends in "]

--- examples/simple_example/simple_example_main.cpp
+++ examples/simple_example/simple_example_main.cpp
@@ -14,16 +14,26 @@ int main(void)
 		}
 	}, Arg<std::vector<std::vector<int>>>("arg_name"));
 
+//	s.registerCommand("name", "disc", [](bool v){
+//		std::cout << (v ? "true":"false") << std::endl;
+//	}, Arg<bool>("arg_name"));
+
 //	s.registerCommand("name1", "disc", [](std::vector<std::vector<int>> v){
 //		std::cout << v.size();
 //	}, Arg<std::vector<std::vector<int>>>("arg_name"));
 
 //	String str1 = "[ [1 2 3] [4 5 6] [7 8 9] ]";
-//	String str2 = "[[0] [1]]";
-	String str2 = "[ [1]]";
+	String str2 = "[[0] [1]]";
+	String str21 = "[ [0] [1] ]";
+	String str22 = "[[0 2 3] [1 2 3]]";
+	String str222 = "[]";
+//	String str2 = "true";
 //	String str3 = "[[1 2 3] [4 5 6] [7 8 9]]";
 //	std::cout << std::endl << s.commands()["name"]->operator()(str1).m_Data;
 	std::cout << std::endl << s.commands()["name"]->operator()(str2).m_Data;
+	std::cout << std::endl << s.commands()["name"]->operator()(str21).m_Data;
+	std::cout << std::endl << s.commands()["name"]->operator()(str22).m_Data;
+	std::cout << std::endl << s.commands()["name"]->operator()(str222).m_Data;
 //	std::cout << std::endl << s.commands()["name"]->operator()(str3).m_Data;
 
 //	std::cout << s.commands()["name"]->Help();
--- include/ccli/ccli_argument_parser.h
+++ include/ccli/ccli_argument_parser.h
@@ -12,17 +12,21 @@
 #include <vector>
 #include <stdexcept>
 
-namespace ccli {
+namespace ccli
+{
 	// unary argument
 	template<typename T>
-	struct CCLI_API ArgumentParser {
+	struct CCLI_API ArgumentParser
+	{
 		inline ArgumentParser(String &input, unsigned long &start);
 
 		T m_Value;
 	};
 
 	template<typename T>
-	inline ArgumentParser<T>::ArgumentParser(String &input, unsigned long &start) {
+	inline ArgumentParser<T>::ArgumentParser(String &input, unsigned long &start)
+	{
+//		static_assert(, "Unsupported type T"); // ADD THIS SO PEOPLE DONT USE THEIR OWN TYPES
 		throw ArgumentException("Unsupported type: " + std::string(typeid(T).name()));
 	}
 
@@ -49,7 +53,8 @@ namespace ccli {
                                                                     + TYPE_NAME + " argument", arg); } \
   }
 
-	ARG_PARSE_BASE_SPEC(ccli::String) {
+	ARG_PARSE_BASE_SPEC(ccli::String)
+	{
 		ccli::String result;
 		auto range = input.NextPoi(start);
 		if (input.m_String[range.first] != '"')
@@ -57,7 +62,9 @@ namespace ccli {
 		else
 			++range.first;
 
-		while (true) {
+		// make ending in ] okay so like "]
+		while (true)
+		{
 			m_Value.m_String += input.m_String.substr(range.first, range.second);
 			// at the end
 			if (m_Value.m_String[m_Value.End() - 2] == '\"')
@@ -71,8 +78,8 @@ namespace ccli {
 		m_Value.m_String.pop_back();
 	}
 
-
-	ARG_PARSE_BASE_SPEC(bool) {
+	ARG_PARSE_BASE_SPEC(bool)
+	{
 		static const char *s_err_msg = "Missing or invalid boolean argument";
 		static const char *s_true = "true\0";
 		static const char *s_false = "false";
@@ -86,18 +93,22 @@ namespace ccli {
 			throw ArgumentException(s_err_msg, arg);
 
 		// make all letters lower case
-		for (size_t i = 0, len = arg.length(); i < len; ++i) {
+		for (size_t i = 0, len = arg.length(); i < len; ++i)
+		{
 			arg[i] = char(std::tolower(arg[i]));
 
 			// if the current letter is not a letter of "true" or "false", error out
-			if (arg[i] != s_true[i] || arg[i] != s_false[i])
+			if (arg[i] != s_true[i] && arg[i] != s_false[i])
 				throw ArgumentException(s_err_msg, input.m_String.substr(range.first, range.second));
 		}
 
-		m_Value = arg == "true";
+		if (arg == "true") m_Value = true;
+		else if (arg == "false") m_Value = false;
+		else ArgumentException(s_err_msg, input.m_String.substr(range.first, range.second));
 	}
 
-	ARG_PARSE_BASE_SPEC(char) {
+	ARG_PARSE_BASE_SPEC(char)
+	{
 		auto range = input.NextPoi(start);
 		std::string arg = input.m_String.substr(range.first, range.second);
 		// must follow '<char>' convention
@@ -107,7 +118,8 @@ namespace ccli {
 			throw ArgumentException("Missing or invalid char argument", arg);
 	}
 
-	ARG_PARSE_BASE_SPEC(unsigned char) {
+	ARG_PARSE_BASE_SPEC(unsigned char)
+	{
 		auto range = input.NextPoi(start);
 		std::string arg = input.m_String.substr(range.first, range.second);
 		// must follow '<char>' convention
@@ -160,7 +172,8 @@ namespace ccli {
 
 	// vector arguments, syntax = [arg1 arg2 arg3 ... argN]
 	template<typename T>
-	struct CCLI_API ArgumentParser<std::vector<T>> {
+	struct CCLI_API ArgumentParser<std::vector<T>>
+	{
 		ArgumentParser(String &input, unsigned long &start);
 
 		std::vector<T> m_Value;
@@ -170,31 +183,48 @@ namespace ccli {
 	// if [arg, strip [ and pass in arg, call
 
 	template<typename T>
-	ArgumentParser<std::vector<T>>::ArgumentParser(String &input, unsigned long &start) {
+	ArgumentParser<std::vector<T>>::ArgumentParser(String &input, unsigned long &start)
+	{
+		// case 1, [something]
+		// case 2, [something something1]
+
 		auto range = input.NextPoi(start);
 		if (range.first == input.End() - 1) return;
 		if (input.m_String[range.first] != '[')
 			throw ArgumentException("Invalid vector argument missing [ delimiter before",
 															input.m_String.substr(range.first, range.second));
+
 		input.m_String[range.first] = ' ';
-		start = range.first + 1;
-		std::cout << input.m_String << std::endl;
-		while (range.first < input.End() - 1) // maybe not -1
+
+		range = input.NextPoi(range.first);
+		start = range.first;
+
+		std::cout << input.m_String << " : [start] = " << input.m_String[start] << std::endl;
+		while (range.first < input.End() - 1)
 		{
-			if (input.m_String[range.second - 1] == ']') {
+			start = range.first;
+			std::cout << input.m_String << " : [start] = " << input.m_String[start] << std::endl;
+
+			if (input.m_String[start] == '[')
+			{
+				m_Value.push_back(ArgumentParser<T>(input, start).m_Value);
+			}
+			else if (input.m_String[range.second - 1] == ']')
+			{
+				if (range.first == range.second - 1) return;
 				while (input.m_String[range.second - 1] == ']' && range.first != range.second)
 					--range.second;
 				input.m_String[range.second] = ' ';
-				std::cout << input.m_String << std::endl;
-				start = range.first;
+
+			  std::cout << input.m_String << " : [start] = " << input.m_String[start] << std::endl;
 				m_Value.push_back(ArgumentParser<T>(input, start).m_Value);
 				return;
 			}
-			else {
+			else
+			{
 				m_Value.push_back(ArgumentParser<T>(input, start).m_Value);
-				std::cout << input.m_String << std::endl;
-				range = input.NextPoi(start);
 			}
+			range = input.NextPoi(start);
 		}
 	}
 }

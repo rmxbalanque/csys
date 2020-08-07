49d571e755985c366ad92828e633d526baf33f16
Tristan <tristan.bouchard@digipen.edu>
2020-08-07 16:24:31 -0700

    fix:Added null char checks for the next poi  [skip ci]
    __DESCRIPTION:__
    in string.h, the || needed to be an &&

--- include/csys/string.h
+++ include/csys/string.h
@@ -76,7 +76,7 @@ namespace csys
 
             // Go to the first non-whitespace char
             for (; pos < end; ++pos)
-                if (!std::isspace(m_String[pos]))
+                if (!std::isspace(m_String[pos]) && m_String[pos] != '\0')
                 {
                     range.first = pos;
                     break;
